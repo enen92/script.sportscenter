fa331411005c5c88b544eae85053dd4cb63b20a2
enen92 <92enen@gmail.com>
2015-04-27 20:53:04 +0100

    sync progress eventlist

--- resources/lib/eventlist.py
+++ resources/lib/eventlist.py
@@ -12,6 +12,7 @@ from centerutils.common_variables import *
 from centerutils.database import sc_database
 from centerutils import pytzimp
 from random import randint
+from centerutils.datemanipulation import *
 import homemenu as home
 import thesportsdb
 import leagueview as leagueview
@@ -109,16 +110,58 @@ class dialog_eventlist(xbmcgui.WindowXML):
 				event_thumb = thesportsdb.Events().get_thumb(event)
 				event_poster = thesportsdb.Events().get_poster(event)
 				event_sport = thesportsdb.Events().get_sport(event)
+				event_season = thesportsdb.Events().get_season(event)
+				event_label = ''
+				
+				#plot
+				event_plot = thesportsdb.Events().get_plot(event)
+				if not event_plot or event_plot == 'None' or event_plot == 'null' or settings.getSetting('hide-results') == 'true':
+					event_plot = ''
+					
+				#season
+				if len(event_season) == 4 and event_season != 'None' and event_season != 'null':
+					if int(event_season) < 1900:
+						i = 0
+						start_year=''
+						end_year=''
+						for char in event_season:
+							if i == 0 or i == 1: start_year = start_year + char
+							else: end_year = end_year + char
+							i+=1
+						if int(start_year) > 30 and int(start_year) < 1900: start_year = '19'+start_year
+						else: start_year = '20'+start_year
+						if int(end_year) > 30 and int(end_year) < 1900: end_year = '19'+end_year
+						else: end_year = '20'+end_year
+						season_label = start_year + '/' + end_year
+					else: season_label = event_season
+				else: season_label = event_season
+				
+				print "fucking season library",season_label
+					
+				#round
+				event_round = thesportsdb.Events().get_round(event)
+				if not event_round or event_round == 'null' or event_round == 'None' or event_round == '0':
+					event_round = 'Round: N/A'
+				else:
+					event_round = 'Round: '+str(event_round)
 				
 				#League related data
 				event_leagueid = thesportsdb.Events().get_leagueid(event)
 				event_league_dict = sc_database.Retriever().get_all_leagues(event_sport,event_leagueid)[0]
-								
+				event_league_name = thesportsdb.Leagues().get_name(event)
+				event_league_trophy = thesportsdb.Leagues().get_trophy(event_league_dict)
+				
+				if not event_league_trophy or event_league_trophy == 'None' or event_league_trophy == 'null':
+					event_league_trophy = ''
+					
+					
+				#Properties dependent on the kind of event				
 				if event_race and event_race != 'null':
 					isRace = True
 					home_team_logo = os.path.join(addonpath,art,'raceflag.png')
 					event_name = thesportsdb.Events().get_eventtitle(event)
 					event_round = ''
+					event_label = event_name
 				else:
 					home_team_id = thesportsdb.Events().get_hometeamid(event)
 					home_team_dict = sc_database.Retriever().get_all_teams(None,None,home_team_id)[0]
@@ -142,9 +185,10 @@ class dialog_eventlist(xbmcgui.WindowXML):
 					#check if result is None-None and if so define vs instead of result 
 					if result == 'None-None' or result == 'null-null' or settings.getSetting('hide-results') == 'true': event_vs_present = 'vs'
 					else: event_result_present = result
+					event_label = home_team_name + ' ' + event_vs_present + event_result_present + ' ' + away_team_name
 				
 				#Set event fanart here
-				if not event_fanart or event_fanart != 'None' or event_fanart != 'null':
+				if not event_fanart or event_fanart == 'None' or event_fanart == 'null':
 					if settings.getSetting('event-fanart-priority') != '4':
 						if settings.getSetting('event-fanart-priority') == '0':
 							if not isRace:
@@ -178,6 +222,15 @@ class dialog_eventlist(xbmcgui.WindowXML):
 							league_fanartlist = thesportsdb.Leagues().get_fanart(event_league_dict)
 							if league_fanartlist:
 								event_fanart = league_fanartlist[randint(0,len(league_fanartlist)-1)]
+								
+				#thumb check is done here
+				if not event_thumb or event_thumb == 'None' or event_thumb == 'null':
+					event_thumb = event_fanart
+					
+				#poster check is done here
+				if not event_poster or event_poster == 'None' or event_poster == 'null':
+					event_poster = event_fanart
+					
 				
 				if event_datetime:
 					try:
@@ -185,16 +238,23 @@ class dialog_eventlist(xbmcgui.WindowXML):
 						month = get_month_long(event_datetime.month)
 						year = str(event_datetime.year)
 						extensiveday = '%s %s %s' % (day,month,year)
-						fmt = "%H:%M"
-						extensivetime=event_datetime.strftime(fmt)
-						extensiveday = '%s %s %s' % (day,month,year)
-						event_timestring = extensiveday + ' - ' + extensivetime
+						event_timestring = extensiveday
 					except: event_timestring = ''
 				else: event_timestring = ''
-				
-				game = xbmcgui.ListItem(event_fullname)
+								
+				game = xbmcgui.ListItem(event_label)
 				game.setProperty('HomeTeamLogo',home_team_logo)
 				game.setProperty('fanart',event_fanart)
+				game.setProperty('event_thumb',event_thumb)
+				game.setProperty('event_poster',event_poster)
+				game.setProperty('event_plot',event_plot)
+				game.setProperty('event_league_name',event_league_name)
+				game.setProperty('event_league_trophy',event_league_trophy)
+				game.setProperty('event_timestring',event_timestring)
+				game.setProperty('event_round',event_round)
+				game.setProperty('event_season',season_label)
+
+
 				if event_year:
 					game.setProperty('event_year',event_year)
 				#banner
@@ -220,7 +280,6 @@ class dialog_eventlist(xbmcgui.WindowXML):
 				game.setProperty('date',event_timestring)
 				self.list_listitems.append(game)
 		
-		print self.list_listitems	
 		xbmc.sleep(200)	
 		self.getControl(self.controler).addItems(self.list_listitems)
 			
@@ -230,8 +289,7 @@ class dialog_eventlist(xbmcgui.WindowXML):
 		xbmc.executebuiltin("SetProperty("+self.preferred_view+",1,home)")
 		self.getControl(2).setLabel(self.preferred_label)
 		xbmc.sleep(100)
-		
-		
+
 		#select 1st item
 		self.setFocusId(self.controler)
 		self.getControl(self.controler).selectItem(0)
@@ -278,28 +336,37 @@ class dialog_eventlist(xbmcgui.WindowXML):
 		except:seleccionado = ''
 	          
 		if seleccionado:
-
 			try: self.getControl(934).setLabel('[B]'+seleccionado.getLabel()+'[/B]')
 			except:pass
-			try: self.getControl(935).setLabel(seleccionado.getProperty('year'))
+			try: self.getControl(935).setLabel(seleccionado.getProperty('event_timestring'))
 			except: pass
-			try: self.getControl(936).setLabel(seleccionado.getProperty('country'))
+			try: self.getControl(936).setLabel(seleccionado.getProperty('event_league_name'))
 			except: pass
-			try: self.getControl(938).setText(seleccionado.getProperty('plot'))
+			try: self.getControl(958).setLabel('[COLOR labelheader]League:[/COLOR][CR]' + seleccionado.getProperty('event_league_name'))
+			except: pass
+			try: self.getControl(937).setLabel(seleccionado.getProperty('event_season') + ' ' + seleccionado.getProperty('event_round'))
+			except: pass
+			try: self.getControl(959).setLabel('[COLOR labelheader]Season & Round:[/COLOR][CR]' + seleccionado.getProperty('event_season') + ' ' + seleccionado.getProperty('event_round'))
+			except: pass
+			try: self.getControl(960).setLabel('[COLOR labelheader]Event date:[/COLOR][CR]' + seleccionado.getProperty('event_timestring'))
+			except: pass
+			try: self.getControl(938).setText(seleccionado.getProperty('event_plot'))
+			except: pass
+			try: self.getControl(70).setText(seleccionado.getProperty('event_plot'))
 			except: pass
 			try: self.getControl(912).setImage(seleccionado.getProperty('fanart'))
 			except: pass
-			try: self.getControl(911).setImage(seleccionado.getProperty('fanart'))
+			try: self.getControl(911).setImage(seleccionado.getProperty('event_thumb'))
 			except: pass
-			try: self.getControl(910).setImage(seleccionado.getProperty('trophy'))
+			try: self.getControl(913).setImage(seleccionado.getProperty('event_thumb'))
 			except: pass
-			try: self.getControl(954).setImage(seleccionado.getProperty('trophy'))
+			try: self.getControl(956).setImage(seleccionado.getProperty('event_poster'))
 			except: pass
-			try: self.getControl(908).setImage(seleccionado.getProperty('clear'))
+			try: self.getControl(957).setImage(seleccionado.getProperty('event_poster'))
 			except: pass
-			try: self.getControl(938).setImage(seleccionado.getProperty('badge'))
+			try: self.getControl(910).setImage(seleccionado.getProperty('event_league_trophy'))
 			except: pass
-			try: self.getControl(939).setImage(seleccionado.getProperty('badge'))
+			try: self.getControl(954).setImage(seleccionado.getProperty('event_league_trophy'))
 			except: pass
 		return
 				
